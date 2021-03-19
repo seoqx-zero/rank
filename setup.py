5b0e0df1fcfb6ce5d3f70ef14a3c8ee956fc9e92
seoqx-zero <80934464+seoqx-zero@users.noreply.github.com>
2021-03-19 10:53:38 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rank',
+    version='v1.0',
+    py_modules=['rank'],
+    author='zero',
+    author_email='zero@seoqx.com',
+    url='https://github.com/seoqx-zero/rank',
+    # requires=['requests','urllib3'],
+    license='MIT'
+)
